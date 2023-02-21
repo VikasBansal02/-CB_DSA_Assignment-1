6473bd0cbc698f74d297273722855a3f3a2c4946
Vikas Bansal <97593886+VikasBansal02@users.noreply.github.com>
2023-02-21 16:54:57 +0530

    Maximum Depth of Binary Tree

---
+++ Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Main {
+
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		BinaryTree bt = m.new BinaryTree();
+		System.out.println(bt.depth());
+	}
+
+	private class BinaryTree {
+		private class Node {
+			int data;
+			Node left;
+			Node right;
+		}
+
+		private Node root;
+		private int size;
+
+		public BinaryTree() {
+			this.root = this.takeInput(null, false);
+		}
+
+		public Node takeInput(Node parent, boolean ilc) {
+
+			int cdata = scn.nextInt();
+			Node child = new Node();
+			child.data = cdata;
+			this.size++;
+
+			// left
+			boolean hlc = scn.nextBoolean();
+
+			if (hlc) {
+				child.left = this.takeInput(child, true);
+			}
+
+			// right
+			boolean hrc = scn.nextBoolean();
+
+			if (hrc) {
+				child.right = this.takeInput(child, false);
+			}
+
+			// return
+			return child;
+		}
+
+		public int depth() {
+			return this.depth(this.root);
+		}
+
+		private int depth(Node root) {
+			// write your code here
+			if(root==null){
+				return 0;
+			}
+			int left=depth(root.left);
+			int right=depth(root.right);
+			return Math.max(left,right)+1;
+		}
+
+	}
+
+}
